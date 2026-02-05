99d20558a148552758b46c5203e07c4653a3c2c1
Karina <Khernandez255@student.rccd.edu>
2026-02-04 20:11:33 -0800

    Update student names

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main() {
 	*/
 	//----------------------------------------------------------------------------------------
     students[0] = {101, "Bill Jones", 87.3};
-    students[1] = {107, "Thomas ", 96.7};
+    students[1] = {107, "Thomas Perez", 96.7};
     students[2] = {110, "Sarah Palmer", 89.4};
     students[3] = {109, "Karol Mendez", 84.8};
     students[4] = {105, "Ethan Johnson", 95.8};
